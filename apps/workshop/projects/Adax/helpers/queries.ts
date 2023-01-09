f9e139165312e042420f29985196b42d5902c876
x-ror <16686943+x-ror@users.noreply.github.com>
2023-01-09 14:49:22 +0200

    refactor(adax): added types for queries

--- apps/workshop/projects/Adax/helpers/queries.ts
+++ apps/workshop/projects/Adax/helpers/queries.ts
@@ -1,5 +1,46 @@
-export async function accountAssets(axios: any, stakeAddress: string) {
+import type { Context } from '@defiyield/sandbox';
+
+interface IAccountAsset {
+  stake_address: string;
+  asset_list: {
+    policy_id: string;
+    asset_name: string;
+    fingerprint: string;
+    quantity: string;
+  }[];
+}
+
+interface IPool {
+  pool_name: string;
+  pool_nft_name: string;
+  pool_lp_amount: number;
+  asset_a: IToken;
+  asset_b: IToken;
+}
+
+interface IToken {
+  ticker: string;
+  name: string;
+  symbol: string;
+  amount: number;
+  metadata: {
+    metadata_decimals: number;
+  };
+}
+
+export async function accountAssets(
+  { axios }: Pick<Context, 'axios'>,
+  stakeAddress: string,
+): Promise<IAccountAsset | void> {
   const body = { ['_stake_addresses']: [stakeAddress] };
   const { data } = await axios.post('https://api.koios.rest/api/v0/account_assets', body);
   return data[0];
+}
+
+export async function getPools({ axios }: Pick<Context, 'axios'>): Promise<IPool[]> {
+  const {
+    data: { pools },
+  } = await axios.post('https://amm-api.adax.pro', { endpoint: 'getPools' });
+
+  return pools;
 }
--- apps/workshop/projects/Adax/modules/AdaxLiquidity.ts
+++ apps/workshop/projects/Adax/modules/AdaxLiquidity.ts
@@ -1,7 +1,7 @@
 import type { ModuleDefinitionInterface, TokenDetail, TokenExtra } from '@defiyield/sandbox';
 import { CARDANO_COIN_ADDRESS } from '../../../common/constants/cardano.constants';
 import { ADAX_POLICY, AVAILABLE_POOLS } from '../helpers/constants';
-import { accountAssets } from '../helpers/queries';
+import { accountAssets, getPools } from '../helpers/queries';
 
 export const AdaxLiquidity: ModuleDefinitionInterface = {
   name: 'AdaxLiquidity',
@@ -28,52 +28,49 @@ export const AdaxLiquidity: ModuleDefinitionInterface = {
   },
 
   async fetchMissingTokenPrices({ assets, axios, BigNumber, allAssets }) {
-    const {
-      data: { pools },
-    } = await axios.post('https://amm-api.adax.pro', { endpoint: 'getPools' });
-
+    const pools = await getPools({ axios });
     const adaToken = allAssets.find((token) => token.address === CARDANO_COIN_ADDRESS);
-    const poolMap = new Map(
-      pools.map((pool: any) => [`${ADAX_POLICY}.${pool.pool_nft_name}`, pool]),
-    );
+    const poolMap = new Map(pools.map((pool) => [`${ADAX_POLICY}.${pool.pool_nft_name}`, pool]));
 
     return assets
       .filter((token) => token.address.startsWith(ADAX_POLICY))
-      .map((token) => {
-        const pool: any = poolMap.get(token.address);
+      .reduce((pools: any[], token) => {
+        const pool = poolMap.get(token.address);
+        if (!pool) {
+          return pools;
+        }
         const secondTokenAddress = `${pool.asset_b.symbol}.${pool.asset_b.name}`;
         const secondToken = allAssets.find((token) => token.address === secondTokenAddress);
-        let tvl = new BigNumber(0);
-        let price = new BigNumber(0);
 
-        if (adaToken!.price && secondToken!.price) {
-          const amountA = new BigNumber(pool.asset_a.amount).div(
-            10 ** pool.asset_a.metadata.metadata_decimals,
-          );
-          const amountB = new BigNumber(pool.asset_b.amount).div(
-            10 ** pool.asset_b.metadata.metadata_decimals,
-          );
+        if (adaToken?.price && secondToken?.price) {
+          let price = new BigNumber(0);
+          let tvl = new BigNumber(0);
+          const amountA = new BigNumber(pool.asset_a.amount) //
+            .div(10 ** pool.asset_a.metadata.metadata_decimals);
+          const amountB = new BigNumber(pool.asset_b.amount) //
+            .div(10 ** pool.asset_b.metadata.metadata_decimals);
 
-          tvl = amountA.times(adaToken!.price).plus(amountB.times(secondToken!.price));
+          tvl = amountA.times(adaToken.price).plus(amountB.times(secondToken.price));
           price = tvl.div(pool.pool_lp_amount);
-        }
 
-        return {
-          address: token.address,
-          price: price.toNumber(),
-          underlying: [
-            {
-              address: CARDANO_COIN_ADDRESS,
-              reserve: Number(pool.asset_a.amount).toString(),
-            },
-            {
-              address: secondTokenAddress,
-              reserve: Number(pool.asset_b.amount).toString(),
-            },
-          ],
-          totalSupply: Number(pool.pool_lp_amount).toString(),
-        };
-      });
+          pools.push({
+            address: token.address,
+            price: price.toNumber(),
+            underlying: [
+              {
+                address: CARDANO_COIN_ADDRESS,
+                reserve: Number(pool.asset_a.amount).toString(),
+              },
+              {
+                address: secondTokenAddress,
+                reserve: Number(pool.asset_b.amount).toString(),
+              },
+            ],
+            totalSupply: Number(pool.pool_lp_amount).toString(),
+          });
+        }
+        return pools;
+      }, []);
   },
 
   async fetchPools({ tokens, BigNumber }) {
@@ -94,20 +91,24 @@ export const AdaxLiquidity: ModuleDefinitionInterface = {
       });
   },
 
-  async fetchUserPositions({ pools, user, axios, cardano }) {
+  async fetchUserPositions({ pools, user, axios, cardano, BigNumber }) {
     const stakeAddress = cardano.getStakeAddress(user);
     if (!stakeAddress) {
       return [];
     }
-    const { asset_list } = await accountAssets(axios, stakeAddress);
+    const result = await accountAssets({ axios }, stakeAddress);
+    const asset_list = result?.asset_list;
     const poolMap = new Map(pools.map((pool) => [pool.id, pool]));
-    if (asset_list?.length) {
+
+    if (Array.isArray(asset_list)) {
       return asset_list.reduce((acc: any, asset: any) => {
         const assetId = `${asset.policy_id}.${asset.asset_name}`;
         const pool = poolMap.get(assetId);
-        if (pool) {
-          const balance = Number(asset.quantity) / 10 ** pool.supplied![0].token.decimals;
-          const supply = Object.assign({ balance }, pool.supplied![0]);
+        if (pool && Array.isArray(pool.supplied) && pool.supplied.length > 0) {
+          const balance = new BigNumber(asset.quantity)
+            .div(10 ** pool.supplied[0].token.decimals)
+            .toNumber();
+          const supply = Object.assign({ balance }, pool.supplied[0]);
           acc.push({
             id: pool.id,
             supplied: [supply],
