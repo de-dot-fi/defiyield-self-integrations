9cc856dcb7be4e22f9476d6812f050bbe6a8c030
Yaroslav Nekryach <80450727+YaroslavNekryach@users.noreply.github.com>
2023-01-23 15:59:36 +0200

    Allbridge. Fixed build issues (#11)
    * Allbridge. Fixed build issues
    
    * Allbridge. Fixed prettier

--- apps/workshop/projects/AllbridgeCore/tests/AllbridgeCore.spec.ts
+++ apps/workshop/projects/AllbridgeCore/tests/AllbridgeCore.spec.ts
@@ -1,9 +1,9 @@
-import {createMockContext, createTestProject, MockContracts} from '@defiyield/testing';
-import {join} from 'path';
-import {describe, test, expect, beforeEach} from 'vitest';
-import {getAllbridgeModule} from '../modules/AllbridgePool';
-import tokenInfoData from './token-info-data.json'
-import {Token} from '@defiyield/sandbox';
+import { createMockContext, createTestProject, MockContracts } from '@defiyield/testing';
+import { join } from 'path';
+import { describe, test, expect, beforeEach } from 'vitest';
+import { getAllbridgeModule } from '../modules/AllbridgePool';
+import tokenInfoData from './token-info-data.json';
+import { Token } from '@defiyield/sandbox';
 
 const SYSTEM_PRECISION = 1e3;
 const TOKEN_BALANCE_1 = 2.3 * SYSTEM_PRECISION;
@@ -14,12 +14,11 @@ const TOKEN_INFO_URL = 'https://core.api.allbridgecoreapi.net/token-info';
 const mockContracts: MockContracts = {
   '0x179aaD597399B9ae078acFE2B746C09117799ca0': {
     tokenBalance: () => TOKEN_BALANCE_1,
-    userInfo: () => ({lpAmount: LP_AMOUNT_1})
-
+    userInfo: () => ({ lpAmount: LP_AMOUNT_1 }),
   },
   '0xB19Cd6AB3890f18B662904fd7a40C003703d2554': {
     tokenBalance: () => TOKEN_BALANCE_2,
-    userInfo: () => ({lpAmount: LP_AMOUNT_2})
+    userInfo: () => ({ lpAmount: LP_AMOUNT_2 }),
   },
   fallback: {},
 };
@@ -45,7 +44,7 @@ describe('#project #AllbridgeCore #pool', () => {
     });
   });
 
-  test('Fetches all the expected tokens', async ({project}) => {
+  test('Fetches all the expected tokens', async ({ project }) => {
     const [, mockAxios] = createMockContext(mockContracts);
     mockAxios.onGet(TOKEN_INFO_URL).reply(200, tokenInfoData);
     const tokensPerModule = await project.preloadTokens();
@@ -53,77 +52,80 @@ describe('#project #AllbridgeCore #pool', () => {
       [
         createFakeToken(tokenInfoData.BSC.tokens[0].tokenAddress),
         createFakeToken(tokenInfoData.BSC.tokens[1].tokenAddress),
-      ]
-    ])
+      ],
+    ]);
   });
 
-  test('Fetches all the expected pools', async ({project}) => {
+  test('Fetches all the expected pools', async ({ project }) => {
     const [, mockAxios] = createMockContext(mockContracts);
     mockAxios.onGet(TOKEN_INFO_URL).reply(200, tokenInfoData);
     const pools = await project.fetchPools();
 
     expect(pools).toEqual([
-        [
-          {
-            id: tokenInfoData.BSC.tokens[0].poolAddress,
-            supplied: [{
-              "token": createFakeToken(tokenInfoData.BSC.tokens[0].tokenAddress),
-              "tvl": 23, // system precision 3, token price 10
-              "apr": {
-                "year": +tokenInfoData.BSC.tokens[0].apr
-              }
-            }],
-            extra: {"lpRate": +tokenInfoData.BSC.tokens[0].lpRate}
-          },
-          {
-            id: tokenInfoData.BSC.tokens[1].poolAddress,
-            supplied: [{
-              "token": createFakeToken(tokenInfoData.BSC.tokens[1].tokenAddress),
-              "tvl": 34,// system precision 3, token price 10
-              "apr": {
-                "year": +tokenInfoData.BSC.tokens[1].apr
-              }
-            }],
-            extra: {"lpRate": +tokenInfoData.BSC.tokens[1].lpRate}
-          },
-        ]
-      ]
-    )
-  });
-
-  test('Calculates the correct reward apr for active pools', async ({project}) => {
-    const [, mockAxios] = createMockContext(mockContracts);
-    mockAxios.onGet(TOKEN_INFO_URL).reply(200, tokenInfoData);
-      const userPositions = await project.fetchUserPositions('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5');
-      expect(userPositions).toEqual([
+      [
         {
-          "id": tokenInfoData.BSC.tokens[0].poolAddress,
-          "supplied": [
+          id: tokenInfoData.BSC.tokens[0].poolAddress,
+          supplied: [
             {
-              "token": createFakeToken(tokenInfoData.BSC.tokens[0].tokenAddress),
-              "tvl": 23,
-              "apr": {
-                "year": +tokenInfoData.BSC.tokens[0].apr
+              token: createFakeToken(tokenInfoData.BSC.tokens[0].tokenAddress),
+              tvl: 23, // system precision 3, token price 10
+              apr: {
+                year: +tokenInfoData.BSC.tokens[0].apr,
               },
-              "balance": 4.5 * +tokenInfoData.BSC.tokens[0].lpRate
-            }
-          ]
+            },
+          ],
+          extra: { lpRate: +tokenInfoData.BSC.tokens[0].lpRate },
         },
         {
-          "id": tokenInfoData.BSC.tokens[1].poolAddress,
-          "supplied": [
+          id: tokenInfoData.BSC.tokens[1].poolAddress,
+          supplied: [
             {
-              "token": createFakeToken(tokenInfoData.BSC.tokens[1].tokenAddress),
-              "tvl": 34,
-              "apr": {
-                "year": +tokenInfoData.BSC.tokens[1].apr
+              token: createFakeToken(tokenInfoData.BSC.tokens[1].tokenAddress),
+              tvl: 34, // system precision 3, token price 10
+              apr: {
+                year: +tokenInfoData.BSC.tokens[1].apr,
               },
-              "balance": 5.6 * +tokenInfoData.BSC.tokens[1].lpRate
-            }
-          ]
-        }
-      ])
+            },
+          ],
+          extra: { lpRate: +tokenInfoData.BSC.tokens[1].lpRate },
+        },
+      ],
+    ]);
+  });
+
+  test('Calculates the correct reward apr for active pools', async ({ project }) => {
+    const [, mockAxios] = createMockContext(mockContracts);
+    mockAxios.onGet(TOKEN_INFO_URL).reply(200, tokenInfoData);
+    const userPositions = await project.fetchUserPositions(
+      '0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5',
+    );
+    expect(userPositions).toEqual([
+      {
+        id: tokenInfoData.BSC.tokens[0].poolAddress,
+        supplied: [
+          {
+            token: createFakeToken(tokenInfoData.BSC.tokens[0].tokenAddress),
+            tvl: 23,
+            apr: {
+              year: +tokenInfoData.BSC.tokens[0].apr,
+            },
+            balance: 4.5 * +tokenInfoData.BSC.tokens[0].lpRate,
+          },
+        ],
+      },
+      {
+        id: tokenInfoData.BSC.tokens[1].poolAddress,
+        supplied: [
+          {
+            token: createFakeToken(tokenInfoData.BSC.tokens[1].tokenAddress),
+            tvl: 34,
+            apr: {
+              year: +tokenInfoData.BSC.tokens[1].apr,
+            },
+            balance: 5.6 * +tokenInfoData.BSC.tokens[1].lpRate,
+          },
+        ],
+      },
+    ]);
   });
 });
-
-
