5ae101621e832cdd964ecdc0a187c7c8d1577873
phiz <phiz@steaker.com>
2023-04-21 16:35:27 +0800

    Add project: XY Finance

---
+++ apps/workshop/projects/XYFinance/index.ts
@@ -0,0 +1,54 @@
+import type { ProjectDefinitionInterface } from '@defiyield/sandbox';
+import { veXY } from './modules/veXY';
+import { YPool } from './modules/ypools';
+
+const project: ProjectDefinitionInterface = {
+  name: 'XY Finance',
+  categories: ['Cross Chain', 'DEX'],
+  links: {
+    logo: '',
+    url: 'https://app.xy.finance',
+    discord: 'https://discord.com/invite/zHKxJYZ52P',
+    telegram: 'https://t.me/xyfinance',
+    twitter: 'https://twitter.com/xyfinance',
+    // github: '',
+  },
+  modules: [
+    YPool('ethereum', 'ETH'),
+    YPool('binance', 'ETH'),
+    YPool('polygon', 'ETH'),
+    YPool('fantom', 'ETH'),
+    YPool('cronos', 'ETH'),
+    YPool('avalanche', 'ETH'),
+    YPool('arbitrum', 'ETH'),
+    YPool('optimism', 'ETH'),
+    YPool('ethereum', 'USDC'),
+    YPool('binance', 'USDC'),
+    YPool('polygon', 'USDC'),
+    YPool('fantom', 'USDC'),
+    YPool('cronos', 'USDC'),
+    YPool('avalanche', 'USDC'),
+    YPool('kucoin', 'USDC'),
+    YPool('arbitrum', 'USDC'),
+    YPool('optimism', 'USDC'),
+    YPool('moonriver', 'USDC'),
+    YPool('klaytn', 'USDC'),
+    YPool('ethereum', 'USDT'),
+    YPool('binance', 'USDT'),
+    YPool('polygon', 'USDT'),
+    YPool('fantom', 'USDT'),
+    YPool('cronos', 'USDT'),
+    YPool('avalanche', 'USDT'),
+    YPool('kucoin', 'USDT'),
+    YPool('arbitrum', 'USDT'),
+    YPool('optimism', 'USDT'),
+    YPool('moonriver', 'USDT'),
+    YPool('klaytn', 'USDT'),
+    veXY('ethereum'),
+    veXY('binance'),
+    veXY('polygon'),
+    veXY('fantom'),
+  ],
+};
+
+export default project;
---
+++ apps/workshop/projects/XYFinance/tests/xy.spec.ts
@@ -0,0 +1,109 @@
+import { createMockContext, createTestProject, MockContracts } from '@defiyield/testing';
+import { Pool } from '@defiyield/sandbox';
+import { join } from 'path';
+import { describe, test, expect, beforeEach } from 'vitest';
+import { veXY } from '../modules/veXY';
+import { VEXYS } from '../helpers/config';
+
+const TVL = 2000;
+const POSITION = 10000;
+const UNLOCK_TIME = 1675146310;
+const multiplier = 1e18;
+
+const TEST_TOKEN = {
+  address: '0x7777777777777777777777777777777777777777',
+  decimals: 18,
+  price: 10,
+  underlying: [],
+};
+
+const VEXY = VEXYS['ethereum'];
+if (!VEXY) {
+  throw new Error('Unsupported chain');
+}
+
+const mockContracts: MockContracts = {
+  [VEXY.xy]: {
+    balanceOf: () => TVL * multiplier,
+  },
+  [VEXY.veXY]: {
+    locked: () => [POSITION * multiplier, UNLOCK_TIME],
+  },
+  fallback: {},
+};
+
+describe('Project XY Finance @ De.Fi', () => {
+  beforeEach(async (context) => {
+    context.project = await createTestProject({
+      name: 'XYFinance',
+      path: join(__dirname, '../index.ts'),
+      modules: [veXY('ethereum')],
+      contracts: mockContracts,
+    });
+  });
+
+  test('Fetches all the expected tokens', async ({ project }) => {
+    const [tokens] = await project.preloadTokens();
+
+    expect(tokens.length).toEqual(1);
+
+    expect(tokens[0]).toMatchObject({
+      address: VEXY.xy,
+      name: expect.any(String),
+      symbol: expect.any(String),
+      decimals: expect.any(Number),
+    });
+  });
+
+  test('Fetches all the expected pools', async () => {
+    const [mockContext] = createMockContext(mockContracts);
+
+    const veXYPools = await veXY('ethereum').fetchPools({
+      tokens: [TEST_TOKEN],
+      ...mockContext,
+    });
+
+    expect(veXYPools.length).toEqual(1);
+
+    const [pool] = veXYPools as [Pool];
+
+    expect(pool.supplied).toEqual([
+      expect.objectContaining({
+        token: TEST_TOKEN,
+        tvl: TVL * TEST_TOKEN.price,
+        apr: { year: expect.any(Number) },
+      }),
+    ]);
+  });
+
+  test('Fetches user position', async () => {
+    const [mockContext] = createMockContext(mockContracts);
+
+    const positions = await veXY('ethereum').fetchUserPositions({
+      user: '0x0000000000000000000000000000000000000000',
+      pools: [
+        {
+          id: 'veXY-ethereum',
+          supplied: [
+            {
+              token: TEST_TOKEN,
+            },
+          ],
+        },
+      ],
+      ...mockContext,
+    });
+
+    expect(positions.length).toEqual(1);
+
+    const [pool] = positions as [Pool];
+
+    expect(pool.supplied).toEqual([
+      expect.objectContaining({
+        balance: POSITION,
+        token: TEST_TOKEN,
+        unlockTime: UNLOCK_TIME * 1000,
+      }),
+    ]);
+  });
+});
